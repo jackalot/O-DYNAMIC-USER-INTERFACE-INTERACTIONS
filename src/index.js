e910e50bd0c0238c1c990fd8858ee3f434fc3dbe
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-23 09:46:37 -0500

    made a slideshowContainerDiv element

--- src/index.js
+++ src/index.js
@@ -6,3 +6,6 @@ const dropdownDiv = document.createElement('div');
 body.appendChild(dropdownDiv);
 dropdownDiv.id = 'dropdown-div';
 createDropdown(dropdownDiv, 'Drop Down with options ', ['option1', 'option2', 'option3', 'option4', 'option5', 'option6', 'option7', 'option8']);
+const slideshowContainerDiv = document.createElement('div');
+body.appendChild(slideshowContainerDiv);
+slideshowContainerDiv.id = 'slideshowContainer-div';
