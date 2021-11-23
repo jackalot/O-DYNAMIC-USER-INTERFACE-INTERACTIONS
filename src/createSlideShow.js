67dc5a72c180cd273d670f5fce9d3c579f088a77
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-23 10:11:58 -0500

    added css and a class

--- src/createSlideShow.js
+++ src/createSlideShow.js
@@ -1,5 +1,6 @@
 function createSlideShow(parent, imagesArray) {
   const slideShowDiv = document.createElement('div');
+  slideShowDiv.classList.add('slideshow');
   parent.append(slideShowDiv);
 }
 export default createSlideShow;
