fab2c4eacb0bb77819282fcca943078c87a5e793
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-24 07:56:51 -0500

    addded the plan to the script, formatted with eslint and prettier

--- src/createSlideShow.js
+++ src/createSlideShow.js
@@ -2,5 +2,13 @@ function createSlideShow(parent, imagesArray) {
   const slideShowDiv = document.createElement('div');
   slideShowDiv.classList.add('slideshow');
   parent.append(slideShowDiv);
+  function addImagesToSlideshow() {
+    for (let i = 0; i < imagesArray.length; i++) {
+      const newImage = document.createElement('img');
+      newImage.src = imagesArray[i];
+      slideShowDiv.append(newImage);
+    }
+  }
+  addImagesToSlideshow();
 }
 export default createSlideShow;
