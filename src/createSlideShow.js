010529faa8bbf8f223610668d9f1b11af1967d4b
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-24 11:41:26 -0500

    added some logs

--- src/createSlideShow.js
+++ src/createSlideShow.js
@@ -11,10 +11,12 @@ function createSlideShow(parent, imagesArray) {
   };
   function addImagesToSlideshow() {
     const middle = findMiddle(imagesArray);
+    console.log(`middle is: ${middle}`);
     for (let i = 0; i < imagesArray.length; i++) {
       const newImage = new Image(150, 250);
       newImage.src = imagesArray[i];
       slideShowDiv.append(newImage);
+      console.log(`the current images array is ${imagesArray[i]}`);
       if (imagesArray[i] === middle) {
         console.log('hi');
       }
