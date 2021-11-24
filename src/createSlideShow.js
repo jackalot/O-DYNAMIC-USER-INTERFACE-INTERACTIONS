f8f9743986413b4524211f9a6e8efa55dbfd625b
jackalotUVM <gonzalezc9999@gmail.com>
2021-11-24 11:49:46 -0500

    tried to find the index of the middle but it returns -1

--- src/createSlideShow.js
+++ src/createSlideShow.js
@@ -10,14 +10,16 @@ function createSlideShow(parent, imagesArray) {
       arr[ind / 2]];
   };
   function addImagesToSlideshow() {
-    const middle = findMiddle(imagesArray);
-    console.log(`middle is: ${middle}`);
+    const middleElement = findMiddle(imagesArray);
+    const middleNumber = imagesArray.indexOf(middleElement);
+    console.log(`middleElement is: ${middleElement}`);
+    console.log(`middleNumber is ${middleNumber}`);
     for (let i = 0; i < imagesArray.length; i++) {
       const newImage = new Image(150, 250);
       newImage.src = imagesArray[i];
       slideShowDiv.append(newImage);
       console.log(`the current images array is ${imagesArray[i]}`);
-      if (imagesArray[i] === middle) {
+      if (i === middleNumber) {
         console.log('hi');
       }
     }
